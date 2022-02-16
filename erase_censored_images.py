c9eed8f624336e118855565336d5969e1c7e1d1e
ArguablePuma143 <javiermarore@gmail.com>
2022-02-16 20:43:17 +0100

    Encuentra las imágenes que hay que borrar

---
+++ erase_censored_images.py
@@ -0,0 +1,68 @@
+import pickle
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+LABEL_FILE_PATH = "personal/train/labels/"
+IMAGE_FILE_PATH = "personal/train/images/"
+
+NEGRO = [0,0,0]
+
+def plot_single_image(img, title=None, size=(8, 8), cmap='viridis'):
+	plt.figure(figsize=size)
+	plt.xticks([])
+	plt.yticks([])
+	plt.grid(False)
+	plt.imshow(img, cmap=cmap)
+	if title:
+		plt.title(title)
+	plt.show()
+
+def plot_multiple_images(imgs, size=(28, 18), cols=None, cmap='viridis', titles=None):
+  plt.figure(figsize=size)
+  count = len(imgs)
+  if cols is None:
+    cols = count
+  rows = count // cols
+  
+  if count % cols != 0:
+    rows += 1
+
+  for index, img in enumerate(imgs):
+    plt.subplot(rows, cols, index + 1)
+    if titles:
+      title = plt.title(titles[index], )
+      plt.setp(title, color="white")
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(img, cmap=cmap)
+  plt.show()
+
+if __name__ == "__main__":
+
+    raras = []
+    if os.path.isfile("raras.pkl"):
+        with open("raras.pkl", "rb") as f:
+            raras = pickle.load(f)
+    else:
+        for file in tqdm(os.listdir(IMAGE_FILE_PATH)):
+            image = cv2.imread( os.path.join(IMAGE_FILE_PATH, file) )
+            #image = cv2.imread( "personal/train/images/000000000590.jpg" )
+            #label = cv2.imread( "personal/train/labels/000000000590.png" )
+            #print(np.unique(label))
+            black  = np.count_nonzero(np.all(image==NEGRO,axis=2))
+            #plot_multiple_images([image, label * 255])
+            if black > 200:
+                raras.append(file)
+
+        with open("raras.pkl", "wb") as f:
+            pickle.dump(raras, f)
+
+    index = 1
+    image = cv2.imread( os.path.join(IMAGE_FILE_PATH, raras[index]) )
+    label = cv2.imread( os.path.join(LABEL_FILE_PATH, raras[index][:-3] + "png"))
+
+    plot_multiple_images([image, label * 255])
