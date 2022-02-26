e53dd3009ac863ff50daf49c43806ddcec14b6b6
ArguablePuma143 <javiermarore@gmail.com>
2022-02-26 19:10:31 +0100

    refactorizadas funciones de ayuda

--- create_labels.py
+++ create_labels.py
@@ -1,44 +1,16 @@
 import json
 import cv2
-from matplotlib import pyplot as plt
 import numpy as np
 import os
 import tqdm
+
+from helper_functions import plot_single_image, plot_multiple_images
 
 ANNOTATION_FILE_PATH = "personal/result.json/"
 RESULT_FILE_PATH = "personal/train/labels/"
 IMAGE_FILE_PATH = "personal/train/images/"
 
-def plot_single_image(img, title=None, size=(8, 8), cmap='viridis'):
-	plt.figure(figsize=size)
-	plt.xticks([])
-	plt.yticks([])
-	plt.grid(False)
-	plt.imshow(img, cmap=cmap)
-	if title:
-		plt.title(title)
-	plt.show()
 
-def plot_multiple_images(imgs, size=(28, 18), cols=None, cmap='viridis', titles=None):
-  plt.figure(figsize=size)
-  count = len(imgs)
-  if cols is None:
-    cols = count
-  rows = count // cols
-  
-  if count % cols != 0:
-    rows += 1
-
-  for index, img in enumerate(imgs):
-    plt.subplot(rows, cols, index + 1)
-    if titles:
-      title = plt.title(titles[index], )
-      plt.setp(title, color="white")
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(img, cmap=cmap)
-  plt.show()
 
 with open( ANNOTATION_FILE_PATH, "r") as f:
     annotations = json.load(f)
--- erase_censored_images.py
+++ erase_censored_images.py
@@ -10,37 +10,6 @@ IMAGE_FILE_PATH = "personal/final_dataset/images/"
 
 NEGRO = [0,0,0]
 
-def plot_single_image(img, title=None, size=(8, 8), cmap='viridis'):
-	plt.figure(figsize=size)
-	plt.xticks([])
-	plt.yticks([])
-	plt.grid(False)
-	plt.imshow(img, cmap=cmap)
-	if title:
-		plt.title(title)
-	plt.show()
-
-def plot_multiple_images(imgs, size=(28, 18), cols=None, cmap='viridis', titles=None):
-  plt.figure(figsize=size)
-  count = len(imgs)
-  if cols is None:
-    cols = count
-  rows = count // cols
-  
-  if count % cols != 0:
-    rows += 1
-
-  for index, img in enumerate(imgs):
-    plt.subplot(rows, cols, index + 1)
-    if titles:
-      title = plt.title(titles[index], )
-      plt.setp(title, color="white")
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(img, cmap=cmap)
-  plt.show()
-
 if __name__ == "__main__":
 
     raras = []
